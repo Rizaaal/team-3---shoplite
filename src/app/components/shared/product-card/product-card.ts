e675dfb4f4ca33840b8ca0df799f156cf0b81de0
Rizal <gabrieledigrazia@protonmail.com>
2026-03-16 17:56:27 +0100

    feat: add dynamic data for card component

--- src/app/components/shared/product-card/product-card.ts
+++ src/app/components/shared/product-card/product-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
-export class ProductCard {}
+export class ProductCard {
+  product = input<Product>({
+    id: 1,
+    name: 'prodotto',
+    description: 'descrizione',
+    category: 'category',
+    price: 0,
+    qty: 1,
+    imageUrl: 'placeholder.jpg',
+  });
+}
+
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  qty: number;
+  imageUrl: string;
+};

28143f7228211cda8d91a31bdc8d72a9191eb921
Rizal <gabrieledigrazia@protonmail.com>
2026-03-16 18:08:17 +0100

    feat: add product endpoint and populate products on home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { ProductCard } from "../../components/shared/product-card/product-card";
-import { ChiSiamo } from "../../components/shared/chi-siamo/chi-siamo";
+import { Component, computed, inject } from '@angular/core';
+import { ProductCard } from '../../components/shared/product-card/product-card';
+import { ChiSiamo } from '../../components/shared/chi-siamo/chi-siamo';
+import { Api } from '../../services/api';
+import { NgClass } from '../../../../node_modules/@angular/common/types/_common_module-chunk';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +10,18 @@ import { ChiSiamo } from "../../components/shared/chi-siamo/chi-siamo";
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  api = inject(Api);
+
+  products = computed(() => {
+    const products = this.api.products;
+
+    // check if defined
+    if (!products.hasValue()) return [];
+
+    // get first 4 elements
+    const filtered = products.value().filter((_, i) => i < 4);
+
+    return filtered;
+  });
+}
--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, resource } from '@angular/core';
+import { Product } from '../components/shared/product-card/product-card';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Api {}
+export class Api {
+  private baseUrl = 'http://localhost:8080/api';
+
+  products = resource<Product[], string>({
+    loader: async () => {
+      const response = await fetch(`${this.baseUrl}/products`);
+
+      if (!response.ok) {
+        throw new Error('Non è stato possibile caricare i prodotti.');
+      }
+
+      return response.json();
+    },
+  });
+}

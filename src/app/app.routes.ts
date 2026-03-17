0ca3898062d79ff6438c9b0c521b074dbf421b0a
Rizal <gabrieledigrazia@protonmail.com>
2026-03-17 11:24:12 +0100

    feat: add guarded admin route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,18 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Main } from './layout/main/main';
 import { Register } from './pages/register/register';
+import { Admin } from './pages/admin/admin';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: '',
     component: Main,
-    children: [{ path: 'home', component: Home }],
+    children: [
+      { path: 'home', component: Home },
+      { path: 'admin', canActivate: [authGuard], component: Admin },
+    ],
   },
   { path: 'sign-in', component: Login },
   { path: 'sign-up', component: Register },
